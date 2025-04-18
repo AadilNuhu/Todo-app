43c09ff510dcb20085b7d9e37fac1c0cd7c1d6b4
Aadil Nuhu <aadilnuhu122@gmail.com>
2025-04-18 07:46:20 -0700

    refactor: comment out unused PORT variable and clean up code formatting

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const cors = require('cors')
-const PORT = 3000
+// const PORT = 3000
 const todoModel = require('./models/Todo')
 
 const app = express()
@@ -10,21 +10,22 @@ app.use(express.json())
 
 mongoose.connect('mongodb://localhost:27017/test')
 
+// POST: create task
 app.post('/task', (req,res) => {
     const task = req.body.task
-    todoModel.create({
-        task: task
-    })
+    todoModel.create({task: task })
     .then(result => res.json(result))
     .catch(err => res.json(err))
 })
 
+// GET: All tasks
 app.get('/get',(req,res)=>{
     todoModel.find()
     .then(result => res.json(result))
     .catch(err => res.json(err))
 })
 
+// PUT: toggle done status
 app.put('/update/:id', (req,res) => {
     const {id} = req.params;
     todoModel.findById(id)
@@ -36,6 +37,7 @@ app.put('/update/:id', (req,res) => {
     })
 })
 
+// DELETE: Task by ID
 app.delete('/delete/:id', (req,res) => {
     const {id} = req.params;
     todoModel.findByIdAndDelete({_id:id})
@@ -43,7 +45,9 @@ app.delete('/delete/:id', (req,res) => {
     .catch(err => res.json(err)) 
 })
 
-app.listen(PORT, ()=>{
-    console.log(`Server running on port : ${PORT}`);
+// app.listen(PORT, ()=>{
+//     console.log(`Server running on port : ${PORT}`);
     
-})
+// })
+
+module.exports = app;
